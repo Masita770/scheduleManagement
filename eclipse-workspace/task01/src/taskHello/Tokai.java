80af8aa7f06247fca2e7c9d80a03bff6ca98e828
圭紀 <@masuyoshi>
2021-03-06 17:35:31 +0900

    Tokai.javaをfor文とkeySetメソッドを使い、順に出力されるよう書きました。

--- eclipse-workspace/task01/src/taskHello/Tokai.java
+++ eclipse-workspace/task01/src/taskHello/Tokai.java
@@ -8,8 +8,12 @@ public class Tokai {
 		areas.put ("愛知", 755);
 		areas.put("岐阜", 199 );
 		areas.put("三重", 178);
-		System.out.println(areas);
 		
+		for(String key : areas.keySet()){
+			int value = areas.get(key);
+		System.out.println(key + "県の人口は" + value + "万人です"); 
+		
+		}
 
 	}
 
